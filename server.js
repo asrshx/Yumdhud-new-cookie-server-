b6c23016a47e6649be6578ae262387de55f2a34b
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2026-08-17 11:26:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.get("/delete", (req, res) => {
   res.send("Cookie deleted");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
